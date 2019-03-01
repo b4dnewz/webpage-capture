572bad3401d01625110beab9c0955691d07a659a
Filippo Conti <filippo@codekraft.it>
2019-03-01 16:47:51 +0100

    v0.2.0-beta.3
    Added prepublish scripts to avoid publishing a new version without having build the package dist

--- examples/basic.js
+++ examples/basic.js
@@ -2,15 +2,15 @@ import path from 'path';
 import WebpageCapture from '../lib/index';
 
 const capture = new WebpageCapture({
-  outputDir: path.resolve(__dirname, './output')
+	outputDir: path.resolve(__dirname, './output')
 });
 
 (async () => {
-  try {
-    await capture.capture('https://github.com/b4dnewz');
-  } catch (e) {
-    console.error(e);
-  } finally {
-    await capture.close();
-  }
+	try {
+		await capture.capture('https://github.com/b4dnewz');
+	} catch (e) {
+		console.error(e);
+	} finally {
+		await capture.close();
+	}
 })();
--- examples/scripts.js
+++ examples/scripts.js
@@ -2,24 +2,24 @@ import path from 'path';
 import WebpageCapture from '../lib/index';
 
 const capture = new WebpageCapture({
-  outputDir: path.resolve(__dirname, './output')
+	outputDir: path.resolve(__dirname, './output')
 });
 
 (async () => {
-  try {
-    await capture.capture('http://example.com', {
-      outputName: 'example_scripts-original'
-    });
+	try {
+		await capture.capture('http://example.com', {
+			outputName: 'example_scripts-original'
+		});
 
-    await capture.capture('http://example.com', {
-      outputName: 'example_scripts-modified',
-      scripts: [
-        'document.body.style.backgroundColor = "#81c784"'
-      ]
-    });
-  } catch (e) {
-    console.error(e);
-  } finally {
-    await capture.close();
-  }
+		await capture.capture('http://example.com', {
+			outputName: 'example_scripts-modified',
+			scripts: [
+				'document.body.style.backgroundColor = "#81c784"'
+			]
+		});
+	} catch (e) {
+		console.error(e);
+	} finally {
+		await capture.close();
+	}
 })();
